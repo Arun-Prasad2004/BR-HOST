5ae39a985f589f4a8dde5cc671043b3796f28a3f
ARUN <Arun-Prasad2004@users.noreply.github.com>
2026-04-02 23:52:12 +0530

    fix: lowercase 'c' in Judgecases.css import to match Linux file system

--- client/src/JudgeCases.jsx
+++ client/src/JudgeCases.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./JudgeCases.css";
+import "./Judgecases.css";
 
 const JudgeCases = () => {
   const { state } = useLocation();
